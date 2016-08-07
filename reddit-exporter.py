f9a4cdac80122a452b7f05788259634622186d29
thepaperpilot <thepaperpilot@gmail.com>
2016-08-07 16:50:11 -0500

    made it support multiple connections, and automatically closing them and stuff. Plus added loading bars

--- reddit-exporter.py
+++ reddit-exporter.py
@@ -1,49 +1,147 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, redirect
-from praw.errors import HTTPException, OAuthInvalidGrant
-import pprint
-import sys
-import logging
 import json
+import logging
+import os.path
 import praw
+import sys
+import tornado
+from praw.errors import OAuthInvalidGrant
+from praw.handlers import MultiprocessHandler
+from pprint import pprint
+from tornado import gen, web, websocket
 
-app = Flask(__name__)
+class BaseHandler(web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
 
-def readSettings():
+class LinkSocketHandler(websocket.WebSocketHandler):
+    links = []
+
+    thread_pool = tornado.concurrent.futures.ThreadPoolExecutor(4)
+
+    def get_compression_options(self):
+        # Non-None enables compression with default options.
+        return {}
+
+    @gen.coroutine
+    def open(self):
+        self.r = get_reddit_object()
+        self.r.set_access_credentials(scope, self.get_secure_cookie("token"))
+        message = {
+            "message": "init",
+            "name": "/u/" + self.r.user.name.encode('utf-8'),
+            }
+        self.write_message(message)
+        yield self.thread_pool.submit(self.get_saved, self.r.get_me())
+
+    def on_message(self, message):
+        print message
+
+    def update_link(self, link):
+        message = {
+            "message": "link",
+            "content": tornado.escape.to_basestring(self.render_string('link.html', link=link)),
+            }
+        self.write_message(message)
+
+    def get_saved(self, user):
+        for i in user.get_saved(limit=None, time='all'):
+            if i._underscore_names is None: # link posts don't have that attribute, whatever it is
+                link = {
+                    "self": "False",
+                    "url": i.url.encode('utf-8'),
+                    "thumbnail": i.thumbnail.encode('utf-8'),
+                    "title": i.title.encode('utf-8'),
+                    "author": "[deleted]" if not i.author else str(i.author.name),
+                    "subreddit": str(i.subreddit.display_name),
+                    "permalink": i.permalink.encode('utf-8'),
+                    "num_comments": str(i.num_comments),
+                }
+                self.links.extend([link])
+                self.update_link(link)
+            else:
+                submission = self.r.get_submission(submission_id=i.link_id.encode('utf-8')[3:])
+                link = {
+                    "self": "True",
+                    "link_url": i.link_url.encode('utf-8'),
+                    "link_title": i.link_title.encode('utf-8'),
+                    "link_author": i.link_author.encode('utf-8'),
+                    "subreddit": str(i.subreddit.display_name),
+                    "permalink": submission.permalink.encode('utf-8'),
+                    "num_comments": str(submission.num_comments),
+                    "author": "[deleted]" if not i.author else str(i.author.name),
+                    "link_id": i.link_id.encode('utf-8')[3:],
+                    "id": i.id.encode('utf-8'),
+                    "body": i.body_html.encode('utf-8'),
+                }
+                self.links.extend([link])
+                self.update_link(link)
+        message = {
+            "message": "done",
+            }
+        self.write_message(message)
+
+class MainHandler(BaseHandler):
+    @web.authenticated
+    def get(self):
+        try:
+            token = self.get_secure_cookie("token")
+            if token:
+                r.set_access_credentials(scope, token)
+            else:
+                r.get_access_information(self.get_secure_cookie("user"))
+                self.set_secure_cookie("token", r.access_token)
+
+            self.render('index.html', name="/u/" + r.user.name.encode('utf-8'), links=[], js="True")
+        except OAuthInvalidGrant:
+            self.redirect('/auth')
+
+class AuthHandler(BaseHandler):
+    def get(self):
+        code = self.get_arguments('code')
+        if code:
+            self.set_secure_cookie("user", code[0])
+            self.redirect("/")
+        else:
+            url = r.get_authorize_url('reddit-exporter', scope)
+            self.render('authenticate.html', link=url)
+
+def read_settings():
     try:
-        settingsFile = open("reddit-exporter.cfg", "r")
+        settings_file = open("reddit-exporter.cfg", "r")
     except IOError:
         logging.exception("Error opening reddit-exporter.cfg.")
         sys.exit(1)
 
-    settingStr = settingsFile.read()
-    settingsFile.close()
+    settings_str = settings_file.read()
+    settings_file.close()
 
     try:
-        settings = json.loads(settingStr)
+        settings = json.loads(settings_str)
     except ValueError:
         logging.exception("Error parsing settings.json.")
         sys.exit(1)
 
-    if (len(settings["client_id"]) == 0):
+    if len(settings["client_id"]) == 0:
         logging.critical("ID not set.")
         sys.exit(1)
 
-    if (len(settings["client_secret"]) == 0):
+    if len(settings["client_secret"]) == 0:
         logging.critical("Secret not set.")
         sys.exit(1)
 
-    if (len(settings["redirect_uri"]) == 0):
+    if len(settings["redirect_uri"]) == 0:
         logging.critical("Redirect URI not set.")
         sys.exit(1)
 
     return settings
 
-def getRedditObject():
+def get_reddit_object():
     r = praw.Reddit('Reddit Saved Links Exporter by /u/ThePaperPilot ver 0.1 see '
                     'https://github.com/thepaperpilot/RedditSavedLinksExporter '
-                    'for source')
+                    'for source',
+                    handler=handler)
 
     r.set_oauth_app_info(client_id=settings['client_id'],
                          client_secret=settings['client_secret'],
@@ -51,64 +149,28 @@ def getRedditObject():
 
     return r
 
-def getCSV(user):
-    json = ''
-    csv = ''
-    md = ''
-    html = '<div style="text-align: center;font-size: large;padding-top: 60px;">\n<a href="https://www.reddit.com/user/' + user.name.encode('utf-8') + '">/u/' + user.name.encode('utf-8') + '</a>\n</div>\n<div class="container">\n'
-
-    # TODO Send up a static webpage with a loading icon but not the content (at first), and use javascript to iterate over this and add the posts to a div as they become ready, and remove the loader at the end
-    for i in user.get_saved(): # (limit=None, time='all'): # This takes a REALLY long time
-        # pprint(vars(i))
-        if not hasattr(i, 'title'):
-           i.title = i.link_title
-        # TODO JSON, CSV, MD, and HTML
-        # TODO fix subreddit and user links in comment HTML (using /r/ and /u/ shortcuts)
-        if (i._underscore_names == None):
-            html += '<div class="link">\n<a href="' + i.url.encode('utf-8') + '" class="thumbnail"><img class="pic" src="' + ('https://b.thumbs.redditmedia.com/ueGKKytgsoCNMne-N18pP8-_fBAMELl02YrlMrlYG2Y.png' if (i.thumbnail == None) | (i.thumbnail == 'self') | (i.thumbnail == 'default') | (i.thumbnail == '') else i.thumbnail.encode('utf-8')) + '"/></a>\n'
-            html += '<div style="display: inline;">\n<p class="title">\n<a href="' + i.url.encode('utf-8') + '">' + i.title.encode('utf-8') + '</a>\n</p>\n'
-            html += '<p class="tagline">\nsubmitted by <a class="reddit" href="https://www.reddit.com/user/' + str(i.author.name) + '">' + str(i.author.name) + '</a> to <a class="reddit" href="https://www.reddit.com/r/' + str(i.subreddit.display_name) + '">' + str(i.subreddit.display_name) + '</a> with <a class="reddit" href="' + i.permalink.encode('utf-8') + '">' + str(i.num_comments) + ' comments</a>\n</p>\n' + '</div>\n</div>\n'
-        else:
-            submission = r.get_submission(submission_id=i.link_id.encode('utf-8')[3:])
-            html += '<div class="link">\n<p class="tagline" style="margin-top: 16px;">\n<a href="' + i.link_url.encode('utf-8') + '">' + i.link_title.encode('utf-8') + '</a> by <a class="reddit" href="https://www.reddit.com/user/' + i.link_author.encode('utf-8') + '">' + i.link_author.encode('utf-8') + '</a> in <a class="reddit" href="https://www.reddit.com/r/' + str(i.subreddit.display_name) + '">' + str(i.subreddit.display_name) + '</a> with <a class="reddit" href="' + submission.permalink.encode('utf-8') + '">' + str(submission.num_comments) + ' comments</a>\n</p>\n'
-            html += '<p class="tagline">\n<a class="reddit" href="https://www.reddit.com/user/' + str(i.author.name) + '">' + str(i.author.name) + '</a>\t<a href="https://www.reddit.com/comments/' + i.link_id.encode('utf-8')[3:] + '/' + submission.title.encode('utf-8').replace(' ', '_') + '/' + i.id.encode('utf-8') + '/">permalink</a>\n</p>\n'
-            html += '<div class="link" style="margin: 4px;">\n' + i.body_html.encode('utf-8') + '\n</div>\n</div>\n'
-
-    html += '</div>'
-
-    return html
-
-@app.route('/')
-def homepage():
-    if (r.is_oauth_session()):
-        content = getCSV(r.get_me())
-
-        with open('index.html') as html:
-            with open('navbar.html') as navbar:
-                return html.read().replace('{{ content }}', content + navbar.read())
-    else:
-        code = request.args.get('code', '')
-        if (code):
-            try:
-                r.get_access_information(code)
-                return redirect(request.path)
-            except OAuthInvalidGrant:
-                pass
-
-        url = r.get_authorize_url('reddit-exporter', ['identity', 'history'])
-        content = '<a class="button" href=%s>\nAuthenticate\n</a>' % url
-        with open('authenticate.html') as html:
-            return html.read().replace('{{ content }}', content)
-
 if __name__ == "__main__":
     logging.basicConfig()
 
-    try:
-        settings = readSettings()
-        r = getRedditObject()
+    os.system("praw-multiprocess")
+    settings = read_settings()
+    handler = MultiprocessHandler()
+    scope = ['identity', 'history']
+    r = get_reddit_object()
 
-        app.run(debug=True, port=65010)
-    except:
-        logging.exception("Uncaught exception")
+    application = web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/auth", AuthHandler),
+            (r"/linksocket", LinkSocketHandler),
+            ],
+        cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        login_url="/auth",
+        )
+
+    application.listen(65010)
+    tornado.ioloop.IOLoop.current().start()
 
     logging.shutdown()
--- static/links.js
+++ static/links.js
@@ -7,12 +7,36 @@ $(document).ready(function() {
 
 var updater = {
     socket: null,
+    link: 1,
 
     start: function() {
         var url = "ws://" + location.host + "/linksocket";
         updater.socket = new WebSocket(url);
         updater.socket.onmessage = function(event) {
-            updater.showMessage(event.data);
+            json = JSON.parse(event.data);
+            switch (json.message) {
+                case "link":
+                    updater.showMessage(json.content);
+                    break;
+                case "init":
+                    $("#name").href = "https://www.reddit.com" + json.name;
+                    $("#name").innerHTML = json.name;
+                    break;
+                case "done":
+                    $("#navbar").slideDown();
+            }
+        };
+        $("#json").onClick = function(event) {
+            updater.socket.send("json");
+        };
+        $("#csv").onClick = function(event) {
+            updater.socket.send("csv");
+        };
+        $("#md").onClick = function(event) {
+            updater.socket.send("md");
+        };
+        $("#html").onClick = function(event) {
+            updater.socket.send("html");
         };
     },
 
@@ -21,5 +45,7 @@ var updater = {
         node.hide();
         $("#container").append(node);
         node.slideDown();
+        updater.link++;
+        $("#num").text(updater.link);
     }
 };
